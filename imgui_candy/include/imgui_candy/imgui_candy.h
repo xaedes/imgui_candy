706443fa583f1e41a0471fb6ef240ce7cf26fb71
xaedes <xaedes@gmail.com>
2021-12-20 15:44:51 +0100

    refactored ImGuiDataTypeOfPtr to add overloads for signed and unsigned long and long long integers, which may be different type of explicit width integers (int64_t, etc) despite being both integers of the same bit width.

--- imgui_candy/include/imgui_candy/imgui_candy.h
+++ imgui_candy/include/imgui_candy/imgui_candy.h
@@ -5,26 +5,54 @@
 #include <sstream>
 #include <iomanip>
 
-template<class U>
-struct ImGuiDataTypeOfPtr_
+struct ImGuiDataTypeOfPtr
 {
-    template <class T> ImGuiDataType operator()( T*   val );
-    template <> ImGuiDataType operator()( int8_t*   val ) { return ImGuiDataType_S8;     }
-    template <> ImGuiDataType operator()( uint8_t*  val ) { return ImGuiDataType_U8;     }
-    template <> ImGuiDataType operator()( int16_t*  val ) { return ImGuiDataType_S16;    }
-    template <> ImGuiDataType operator()( uint16_t* val ) { return ImGuiDataType_U16;    }
-    template <> ImGuiDataType operator()( int32_t*  val ) { return ImGuiDataType_S32;    }
-    template <> ImGuiDataType operator()( uint32_t* val ) { return ImGuiDataType_U32;    }
-    template <> ImGuiDataType operator()( int64_t*  val ) { return ImGuiDataType_S64;    }
-    template <> ImGuiDataType operator()( uint64_t* val ) { return ImGuiDataType_U64;    }
-    template <> ImGuiDataType operator()( float*    val ) { return ImGuiDataType_Float;  }
-    template <> ImGuiDataType operator()( double*   val ) { return ImGuiDataType_Double; }
+    static ImGuiDataType Call( int8_t*   val ) { return ImGuiDataType_S8;     }
+    static ImGuiDataType Call( uint8_t*  val ) { return ImGuiDataType_U8;     }
+    static ImGuiDataType Call( int16_t*  val ) { return ImGuiDataType_S16;    }
+    static ImGuiDataType Call( uint16_t* val ) { return ImGuiDataType_U16;    }
+    static ImGuiDataType Call( int32_t*  val ) { return ImGuiDataType_S32;    }
+    static ImGuiDataType Call( uint32_t* val ) { return ImGuiDataType_U32;    }
+    static ImGuiDataType Call( int64_t*  val ) { return ImGuiDataType_S64;    }
+    static ImGuiDataType Call( uint64_t* val ) { return ImGuiDataType_U64;    }
+    static ImGuiDataType Call( float*    val ) { return ImGuiDataType_Float;  }
+    static ImGuiDataType Call( double*   val ) { return ImGuiDataType_Double; }
+    
+    template<
+        class T,
+        std::enable_if_t<std::is_same<T,long long>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int32_t>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int64_t>::value, bool> = true
+    >
+    static ImGuiDataType Call( T* val)  { return (sizeof(T) == 32) ? ImGuiDataType_S32 : ImGuiDataType_S64; }
+    template<
+        class T,
+        std::enable_if_t<std::is_same<T,long>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int32_t>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int64_t>::value, bool> = true
+    >
+    static ImGuiDataType Call( T* val)  { return (sizeof(T) == 32) ? ImGuiDataType_S32 : ImGuiDataType_S64; }
+    template<
+        class T,
+        std::enable_if_t<std::is_same<T,unsigned long long>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int32_t>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int64_t>::value, bool> = true
+    >
+    static ImGuiDataType Call( T* val)  { return (sizeof(T) == 32) ? ImGuiDataType_S32 : ImGuiDataType_S64; }
+    template<
+        class T,
+        std::enable_if_t<std::is_same<T,unsigned long>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int32_t>::value, bool> = true,
+        std::enable_if_t<!std::is_same<T,int64_t>::value, bool> = true
+    >
+    static ImGuiDataType Call( T* val)  { return (sizeof(T) == 32) ? ImGuiDataType_S32 : ImGuiDataType_S64; }
 };
+
 
 template<typename T>
 bool ImGuiSliderScalar(const char* label, T* p_data, const void* p_min, const void* p_max, const char* format = NULL, ImGuiSliderFlags flags = 0)
 {
-    return ImGui::SliderScalar(label, ImGuiDataTypeOfPtr_<T>()(p_data), p_data, p_min, p_max, format, flags);
+    return ImGui::SliderScalar(label, ImGuiDataTypeOfPtr::Call(p_data), p_data, p_min, p_max, format, flags);
 }
 template<typename T>
 bool ImGuiSliderScalar(const char* label, T* p_data, T p_min, T p_max, const char* format = NULL, ImGuiSliderFlags flags = 0)
@@ -35,7 +63,7 @@ bool ImGuiSliderScalar(const char* label, T* p_data, T p_min, T p_max, const cha
 template<typename T>
 bool ImGuiSliderScalarN(const char* label, T* p_data, int components, const void* p_min, const void* p_max, const char* format = NULL, ImGuiSliderFlags flags = 0)
 {
-    return ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr_<T>()(p_data), p_data, components, p_min, p_max, format, flags);
+    return ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr::Call(p_data), p_data, components, p_min, p_max, format, flags);
 }
 template<typename T>
 bool ImGuiSliderScalarN(const char* label, T* p_data, int components, T p_min, T p_max, const char* format = NULL, ImGuiSliderFlags flags = 0)
@@ -47,7 +75,7 @@ template<typename T, typename F>
 bool ImGuiSliderScaledScalar(const char* label, T* p_data, F scale, const void* p_scaled_min, const void* p_scaled_max, const char* format = NULL, ImGuiSliderFlags flags = 0)
 {
     F scaled_data = (*p_data) / (scale);
-    bool changed = ImGui::SliderScalar(label, ImGuiDataTypeOfPtr_<F>()(&scaled_data), &scaled_data, p_scaled_min, p_scaled_max, format, flags);
+    bool changed = ImGui::SliderScalar(label, ImGuiDataTypeOfPtr::Call(&scaled_data), &scaled_data, p_scaled_min, p_scaled_max, format, flags);
     if (changed)
     {
         *p_data = static_cast<T>(scaled_data * scale);
@@ -71,7 +99,7 @@ bool ImGuiSliderScaledScalarN(const char* label, T* p_data, int components, cons
     }
     F* p_scaled = &scaled_data[0];
 
-    bool changed = ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr_<F>()(p_scaled), p_scaled, components, p_scaled_min, p_scaled_max, format, flags);
+    bool changed = ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr::Call(p_scaled), p_scaled, components, p_scaled_min, p_scaled_max, format, flags);
     if (changed)
     {
         for (int i=0; i<components; ++i)
@@ -97,7 +125,7 @@ bool ImGuiSliderScaledScalarN(const char* label, T* p_data, int components, cons
     }
     F* p_scaled = &scaled_data[0];
 
-    bool changed = ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr_<F>()(p_scaled), p_scaled, components, p_scaled_min, p_scaled_max, format, flags);
+    bool changed = ImGui::SliderScalarN(label, ImGuiDataTypeOfPtr::Call(p_scaled), p_scaled, components, p_scaled_min, p_scaled_max, format, flags);
     if (changed)
     {
         for (int i=0; i<components; ++i)
